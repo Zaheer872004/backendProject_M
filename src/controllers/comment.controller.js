a45ee4c8808c415beee0ff31c6026e6a9391bc94
Zaheer khan,Zaheer Khan, Habib khan, Habib Khan <zaheerkhan872004@gmail.com,zaheer.224246108@vcet.edu.in,habibkhan15031999@gmail.com,habibkhan19992022@gmail.com>
2024-04-30 12:09:54 +0530

    getvideoComment method is added in comment controller

---
+++ src/controllers/comment.controller.js
@@ -0,0 +1,124 @@
+import mongoose from "mongoose"
+import { Video } from "../models/video.model.js"
+import {Comment} from "../models/comment.model.js"
+import {ApiError} from "../utils/ApiError.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+import {asyncHandler} from "../utils/asyncHandler.js"
+
+const getVideoComments = asyncHandler(async (req, res) => {
+    //TODO: get all comments for a video
+    const {videoId} = req.params
+    const {page = 1, limit = 10} = req.query
+
+    if(!videoId){
+        throw new ApiError(400,`provide the valid videoId`)
+    }
+
+    const video = await Video.findById(videoId);
+
+    if(!video){
+        throw new ApiError(400,`provide the correct videoId`)
+    }
+
+    // comment schema connected to video and user schema.
+
+    const commentAggregate = await Comment.aggregate(
+        [
+            {
+                $match : {
+                   video :  new mongoose.Types.ObjectId(videoId)
+                }
+            },
+            {
+                $lookup : {
+                    from : "users",
+                    localField : "owner",
+                    foreignField : "_id",
+                    as : "owner"
+                }
+            },
+            {
+                $lookup : {
+                    from : "likes",
+                    localField : "_id",
+                    foreignField : "comment",
+                    as : "likes"
+                }
+            },
+            {
+                $addFields : {
+                    likeCount : {
+                        $size : "$likes"
+                    },
+                    owner : {
+                        $first : "$owner"
+                    },
+                    isLiked : {
+                        $cond : { 
+                            if : { $in : [req.user._id, "$likes.likedBy"]},
+                            then : true,
+                            else : false
+                        }
+                    }
+                }
+            },
+            {
+                $sort : {
+                    createdAt : -1
+                }
+            },
+            {
+                $project : {
+                    content : 1,
+                    createdAt : 1,
+                    likeCount : 1,
+                    owner : {
+                        username : 1,
+                        fullName : 1,
+                        "avatar.url" : 1
+                    },
+                    isLiked : 1,
+
+                }
+            }
+        ]
+    );
+    
+    const option =  {
+
+    page : parseInt(page, 10),
+    limit : parseInt(limit, 10)
+
+    }
+
+    const comment = await Comment.aggregatePaginate(
+        commentAggregate,
+        option
+    )
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,comment,`successfully fetch the all comment in this video`)
+    )
+
+})
+
+const addComment = asyncHandler(async (req, res) => {
+    // TODO: add a comment to a video
+})
+
+const updateComment = asyncHandler(async (req, res) => {
+    // TODO: update a comment
+})
+
+const deleteComment = asyncHandler(async (req, res) => {
+    // TODO: delete a comment
+})
+
+export {
+    getVideoComments, 
+    addComment, 
+    updateComment,
+     deleteComment
+}
